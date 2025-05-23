3866f2fa262d8cc37f3a4a1470516a9de8f9bcaf
ub-victor <victoireushindi371@gmail.com>
2025-05-23 04:04:42 +0200

    Initial commit

--- 8-kyu/countPositivesSumNegative.js
+++ 8-kyu/countPositivesSumNegative.js
@@ -1,3 +1,14 @@
 function countPositivesSumNegatives(input) {
-    
+    let result = [0, 0];
+    if (input == null || input.length == 0){
+        return [];
+    }
+
+    for (let i of input){
+        if (i> 0){
+            result[0]++;
+        }else {
+            result[1] += 1;
+        }
+    }return result;
 }
