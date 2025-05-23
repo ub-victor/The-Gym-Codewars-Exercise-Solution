3e7ba941fcc6249ccacf2cf1c36994e464aa037e
ub-victor <victoireushindi371@gmail.com>
2025-05-24 00:40:05 +0200

    Initial commit

--- 8-kyu/objectAssign.js
+++ 8-kyu/objectAssign.js
@@ -1,2 +1,5 @@
 const me = { username: 'JohnDoe' , country: 'USA' };
 
+Object.assign(me, { age: 30, country: 'Canada' }); // { username: 'JohnDoe', country: 'Canada', age: 30 }
+
+console.log(me); //
