51105d9217720125c423dfcba570e383bcab6e57
ub-victor <victoireushindi371@gmail.com>
2025-05-23 04:53:34 +0200

    Initial commit

--- 8-kyu/SortandStar.js
+++ 8-kyu/SortandStar.js
@@ -3,5 +3,3 @@ function twoSort(s) {
    let final = result[0].plit().join("***")
     return final ;
 }
-
-console.log(twoSort(["bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps"]));
