029efdb7246e72b444fa9335e5feca1834a5c2ff
ub-victor <victoireushindi371@gmail.com>
2025-06-01 16:53:39 +0200

    Initial commit

--- 8-kyu/removeSpaces.js
+++ 8-kyu/removeSpaces.js
@@ -1,3 +1,5 @@
 function removeSpaces(str){
     return str.replace(/\s/g, ''); //this regex matches all whitespace characters and replaces them with an empty string so \s is udefined as a whitespace character
 }
+
+console.log(removeSpaces("The quick brown fox jumps over the lazy dog")); 
