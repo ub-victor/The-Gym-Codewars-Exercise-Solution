83b401fd2a598189c2f4d6f585e82b3c08ebf14c
ub-victor <victoireushindi371@gmail.com>
2025-06-01 16:38:12 +0200

    Initial commit

---
+++ 8-kyu/removeSpaces.js
@@ -0,0 +1,3 @@
+function removeSpaces(str){
+    return str.replace(/\s/g, ''); //this regex matches all whitespace characters and replaces them with an empty string so \s is udefined as a whitespace character
+}
