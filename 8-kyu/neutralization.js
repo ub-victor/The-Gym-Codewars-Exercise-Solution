33de82d35f4ef31fae9cda88da2d16567ef9fb69
ub-victor <victoireushindi371@gmail.com>
2025-06-06 16:41:30 +0200

    Initial commit

--- 8-kyu/neutralization.js
+++ 8-kyu/neutralization.js
@@ -1,3 +1,15 @@
 function neutralise(s1, s2) {
-    
+    let result = '';
+    for (let i = 0; i < s1.length; i++) {
+        const c1 = s1[i];
+        const c2 = s2[i];
+        if (c1 === c2) {
+            result += c1;
+        }else {
+            result = '0'
+        }
+    }
 }
+
+console.log(neutralise('abc', 'abc')); // 'abc'
+console.log(neutralise('abc', 'abd')); // '0'
