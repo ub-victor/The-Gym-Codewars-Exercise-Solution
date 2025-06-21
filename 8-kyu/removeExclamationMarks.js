a4a2b8e89b547d00e89139bf5ccb5e323369f0ff
ub-victor <victoireushindi371@gmail.com>
2025-06-21 13:09:39 +0200

    Initial commit

--- 8-kyu/removeExclamationMarks.js
+++ 8-kyu/removeExclamationMarks.js
@@ -1,4 +1,5 @@
 function removeExclamationMarks (s){
   // Use the replace method with a regular expression to remove all exclamation marks
   return s.replace(/!/g, '');
+  // // return s.replaceAll('!', '');
 }
