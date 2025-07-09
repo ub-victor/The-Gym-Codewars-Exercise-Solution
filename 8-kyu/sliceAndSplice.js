3e4cd4a5f1d1e49b66787554744e465bdeef06d5
ub-victor <victoireushindi371@gmail.com>
2025-07-09 12:14:04 +0200

    Initial commit

--- 8-kyu/sliceAndSplice.js
+++ 8-kyu/sliceAndSplice.js
@@ -7,3 +7,10 @@
 
 const log = console.log;
 
+let names = ['John', 'Jane', 'Jack', 'Jill', 'Joe'];
+
+log(names.slice()); // Here the will return a shallow copy of the original array
+
+log(names.slice(1, 3)) // Here the will return a shallow copy of the original array from index 1 to index 3 (not including index 3)
+
+names.splice(1,1, ['James', 'Jasmine', 'Jasper']);
