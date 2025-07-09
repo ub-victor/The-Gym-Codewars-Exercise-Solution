1e726a039c07145072fe4bf9b1359cd769d4c0f9
ub-victor <victoireushindi371@gmail.com>
2025-07-09 11:27:24 +0200

    Initial commit

--- 8-kyu/addNewItemCollectionsArePassedByReference.js
+++ 8-kyu/addNewItemCollectionsArePassedByReference.js
@@ -6,6 +6,11 @@
 // The key point is that the input list should not be modified directly, but instead, we create a new list, copy the contents, add the new item, and return it.
 
 
+/*Spread operator (...) creates a shallow copy of the original array.
+The new array (newList) contains all the elements of listOfNumbers, but they are separate references.
+Changes made to newList (like adding or removing items) won't affect the original listOfNumbers.
+Similarly, if the array contains objects, the objects themselves are not copied but shared (since it's a shallow copy).*/
+
 
 function addExtra(inputList){
     // Create a new list (copy of input list)
