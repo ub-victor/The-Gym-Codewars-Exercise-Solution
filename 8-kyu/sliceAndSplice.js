02a305811d20b21a8ad85cb56e6ae3a3ca478fd1
ub-victor <victoireushindi371@gmail.com>
2025-07-09 12:00:17 +0200

    Initial commit

---
+++ 8-kyu/sliceAndSplice.js
@@ -0,0 +1,5 @@
+// Array.prototype.slice([start, end(not include)])
+// Return a shallow copy of removed/sliced
+
+// Array.prototype.splice(start, deleteCount[, item1, item2, ...])
+// Modify the original array and return the removed elements
