87909cbed9dbbafb859669d3faa471fc678277f0
ub-victor <victoireushindi371@gmail.com>
2025-10-18 22:53:15 +0200

    String to number

--- 8-kyu/numberToString.js
+++ 8-kyu/numberToString.js
@@ -1,5 +1,7 @@
 const numberToString = function(num) {
   return String(num);
+  // return num.toString(); // the toString method can also be used to convert a number to a string
+  // return '' + num;      // concatenating an empty string with the number also converts it to a string
 };
 
 
