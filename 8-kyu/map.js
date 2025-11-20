001444cebf3ffc1a3f54bb220fff98d0839759fc
ub-victor <victoireushindi371@gmail.com>
2025-11-20 10:58:21 +0200

    map exercise

--- 8-kyu/map.js
+++ 8-kyu/map.js
@@ -1,9 +1,12 @@
 const number = [1, -1, 2,3];
 
 const filtered  = number.filter(n=> n>=o);
-
+/*
 const items = filtered.map(n=> "<li>"+ n +"<li>" )
 const html = items.join();
 // const html = "ul"+ items.join("")+ "ul";
+
+*/
+const items 
 
 console.log(items);
