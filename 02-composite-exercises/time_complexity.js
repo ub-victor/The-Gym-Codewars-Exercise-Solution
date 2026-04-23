211e976506a1f103bd62de1f05d06a8f0c41a9a7
ub-victor <victoireushindi371@gmail.com>
2026-04-23 13:17:00 +0200

    Initial commit

--- 02-composite-exercises/time_complexity.js
+++ 02-composite-exercises/time_complexity.js
@@ -3,6 +3,9 @@
 function summation(n){
     const result = []
     for(let i =0; i<=n; i ++ ){
-        
-    } 
+        result.push(i);
+    }
+    return result; 
 }
+
+console.log(summation(10));
