42600c899dd4e059e1a5f03ef1bdab31339e4fe0
ub-victor <victoireushindi371@gmail.com>
2026-05-13 00:18:43 +0200

    code war

--- 7-kyu/applyHeat.js
+++ 7-kyu/applyHeat.js
@@ -26,5 +26,17 @@ const applyHeat = (ingredients) =>{
     // GREEN + YELLOW --> BROWN + YELLOW
     // =========================
 
-    
+    // Check if: 
+    // 1. The array contains exactly 2 items
+    // 2. First chemical is "GREEN"
+    // 3. Second chemical is "YELLOW"
+
+    if (
+        ingredients.lenght === 2 &&
+        ingredients[0] === "GREEN" &&
+        ingredients[1] === "YELLOW"
+    ){
+        // if the input array meet the condition then return the result of the reaction
+        return ["BROWN", "YELLOW"];
+    }
 }
