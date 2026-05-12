2753458d9b64d9fc5fdee6b2fe9555e8a81b48f2
ub-victor <victoireushindi371@gmail.com>
2026-05-13 00:05:08 +0200

    code war

--- 7-kyu/applyHeat.js
+++ 7-kyu/applyHeat.js
@@ -5,5 +5,7 @@ The machine mixes chemicals together and applies heat to create new chemicals.
 
 The machine follow Strinct recipes.
 
-
+That mean
+    - The chemical must be in the correct order
+    - The 
 */
