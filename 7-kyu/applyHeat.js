43161ff03e2d6d53b75b92cde4c41f8fdc471387
ub-victor <victoireushindi371@gmail.com>
2026-05-13 00:06:02 +0200

    code war

--- 7-kyu/applyHeat.js
+++ 7-kyu/applyHeat.js
@@ -8,4 +8,5 @@ The machine follow Strinct recipes.
 That mean
     - The chemical must be in the correct order
     - The machine only understand the recipes provided
+    - If the order is wrong or the chemicals are different, the reaction fails
 */
