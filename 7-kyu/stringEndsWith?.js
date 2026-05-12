56da73d65fad255f7303a524969b38915775ce32
ub-victor <victoireushindi371@gmail.com>
2026-05-12 21:57:08 +0200

    code war

--- 7-kyu/stringEndsWith?.js
+++ 7-kyu/stringEndsWith?.js
@@ -1,3 +1,10 @@
 /**
+Imagine you have two strings
+- The main one = "abc"
+- The ending you want to check = "bc"
 
+You want to know:
+    Doest  "abc" end with "bc"?
+        if yes -> return true
+        if not -> return false
  */
