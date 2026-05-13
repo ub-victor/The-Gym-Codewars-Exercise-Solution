3171ed08920d29cc9701e87a5a847f07e24a6ce5
ub-victor <victoireushindi371@gmail.com>
2026-05-13 10:06:28 +0200

    code war

--- 7-kyu/simulate.js
+++ 7-kyu/simulate.js
@@ -1,3 +1,6 @@
 /*
+Imagine an electronics factory  that builds computer chips and digital machines.
+Before manufacturing the real circuit, the engineers want to simulate circuit using  JavaScript
 
+The circuit receives 4 electrical signals
 */
