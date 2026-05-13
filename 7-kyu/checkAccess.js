4671c72ac48ecb9e81570da8dbb048dc6d82d11f
ub-victor <victoireushindi371@gmail.com>
2026-05-13 09:26:47 +0200

    code war

--- 7-kyu/checkAccess.js
+++ 7-kyu/checkAccess.js
@@ -2,4 +2,12 @@
 Imagine a smart security door inside a laboratory
 
 The door only opens if the correct security code is entered
+
+The machine checks:
+
+    - If the entered code matches the secret code
+    - If it matches -> access is granted
+    -If it does not match -> access in denied
+
+    Your ta
 */ 
