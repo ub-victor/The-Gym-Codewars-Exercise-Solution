799747f44ac38c0fa3e30740bf343482beabef38
ub-victor <victoireushindi371@gmail.com>
2026-05-13 10:07:13 +0200

    code war

--- 7-kyu/simulate.js
+++ 7-kyu/simulate.js
@@ -2,5 +2,6 @@
 Imagine an electronics factory  that builds computer chips and digital machines.
 Before manufacturing the real circuit, the engineers want to simulate circuit using  JavaScript
 
-The circuit receives 4 electrical signals
+The circuit receives 4 electrical signals:
+input1, input2, input3, input4
 */
