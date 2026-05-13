83492a6a19ecdff91443a4fc52ae80476243a0fd
ub-victor <victoireushindi371@gmail.com>
2026-05-13 10:19:07 +0200

    code war

--- 7-kyu/simulate.js
+++ 7-kyu/simulate.js
@@ -5,4 +5,8 @@ Before manufacturing the real circuit, the engineers want to simulate circuit us
 The circuit receives 4 electrical signals:
 input1, input2, input3, input4
 And each input can only be "true" = Electricity ON or "false" = OFF
+
+WHAT THE CIRCUIT CONTAINS
+
+
 */
