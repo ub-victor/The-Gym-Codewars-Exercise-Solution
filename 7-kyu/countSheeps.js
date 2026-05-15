e3683a7a73e2f7666b3894b9a4b3f87de248232f
ub-victor <victoireushindi371@gmail.com>
2026-05-15 22:59:18 +0200

    code war

--- 7-kyu/countSheeps.js
+++ 7-kyu/countSheeps.js
@@ -2,4 +2,9 @@
 We are asked to count many sheep are present in an arraay
 true = The sheep is present, 
 false = the sheep is missing
+If the array contain null, undefined we will considered bad values, so we should NOT count them.
 */
+
+function countSheeps(sheep){
+    
+}
