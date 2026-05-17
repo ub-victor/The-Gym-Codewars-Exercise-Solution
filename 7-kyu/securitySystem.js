06c4ad774f99590c14b7fbe6464fffef942cd358
ub-victor <victoireushindi371@gmail.com>
2026-05-17 15:55:51 +0200

    code war

--- 7-kyu/securitySystem.js
+++ 7-kyu/securitySystem.js
@@ -5,6 +5,10 @@ Before installing the real electronic circuit, engineers want to simulate how th
 
 The alarm system has 4 sensors:
 
-    -   
+    - doorSensor
+    - WindowSensor
+    - MotionSensor
+    - powerSensor
+    And Each sensor
 
 */
