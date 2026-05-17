30e5246fc0b149b7c414d34a2c3ac9236a98a433
ub-victor <victoireushindi371@gmail.com>
2026-05-17 16:00:54 +0200

    code war

--- 7-kyu/securitySystem.js
+++ 7-kyu/securitySystem.js
@@ -17,6 +17,6 @@ What the circuit should do
     To decide whether the alarm should activate, we need to use(||,&&,!)
 Circuit Logic
     The system follows these steps:
-        1.Check
+        1.Check if the door OR window sensor is active
 
 */
