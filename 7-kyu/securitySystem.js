1f43c67e53ae2d387fbda9005a859609d988c9cd
ub-victor <victoireushindi371@gmail.com>
2026-05-17 15:54:26 +0200

    code war

--- 7-kyu/securitySystem.js
+++ 7-kyu/securitySystem.js
@@ -3,6 +3,6 @@ Imagine  a smart alarm system inside a bank.
 
 Before installing the real electronic circuit, engineers want to simulate how the alarm behaves using JavaScript.
 
-
+The alarm system has 4 sensors:
 
 */
