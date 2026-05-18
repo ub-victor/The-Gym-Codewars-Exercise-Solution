8d5837a1fe84982f02e984863fb0ff3145af757d
ub-victor <victoireushindi371@gmail.com>
2026-05-18 15:53:43 +0200

    code war

--- 7-kyu/irrigationSystem.js
+++ 7-kyu/irrigationSystem.js
@@ -4,7 +4,10 @@ Imagine a smart irrigation system used on a modern farm.
 Before building the real eletronic controller, engineers want to simulate how the watering system behaves using JavaScript.
 
 The irrigation system has 4 sensors
-
+    rainSensor
+    soilDry
+    waterTankFull
+    manualOverride
 
 
 */
