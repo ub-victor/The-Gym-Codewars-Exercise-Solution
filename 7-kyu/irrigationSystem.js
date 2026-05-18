17d364c78fcfe7c51674770ee0d38c2c277ba490
ub-victor <victoireushindi371@gmail.com>
2026-05-18 15:37:21 +0200

    code war

--- 7-kyu/irrigationSystem.js
+++ 7-kyu/irrigationSystem.js
@@ -1,5 +1,7 @@
 /*
 Imagine a smart irrigation system used on a modern farm.
 
+Before building the real eletronic controller, engineers want to simulate how the watering system behaves using JavaScript.
+
 
 */
