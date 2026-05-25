0dbced1726af17b273e7c228bf937df09e06be32
ub-victor <victoireushindi371@gmail.com>
2026-05-26 01:11:29 +0200

    reverse function

---
+++ 7-kyu/reverseWords.js
@@ -0,0 +1,2 @@
+// ('The quick brown fox jumps over the lazy dog.'), 'ehT kciuq nworb xof spmuj revo eht yzal .god', `Input: "The quick brown fox jumps over the lazy dog."`);
+// ('apple'), 'elppa', `Input: "apple"`);
