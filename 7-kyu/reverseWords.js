6b85c488d65a8c38301c9d400fa64cf9134284df
ub-victor <victoireushindi371@gmail.com>
2026-05-26 12:07:24 +0200

    reverse function

--- 7-kyu/reverseWords.js
+++ 7-kyu/reverseWords.js
@@ -1,2 +1,11 @@
 // ('The quick brown fox jumps over the lazy dog.'), 'ehT kciuq nworb xof spmuj revo eht yzal .god', `Input: "The quick brown fox jumps over the lazy dog."`);
 // ('apple'), 'elppa', `Input: "apple"`);
+
+const reverseWords = (str)=>{
+    return str
+        .split(" ")
+        .map(word=> word.split("").reverse().join(""))
+        .join(" ");
+}
+
+console.log(reverseWords("he quick brown fox jumps over the lazy dog"))
