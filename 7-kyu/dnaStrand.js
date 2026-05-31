5e08a03f99c7de8cab132abcfca976a53f8601ae
ub-victor <victoireushindi371@gmail.com>
2026-06-01 00:50:52 +0200

    Code war

---
+++ 7-kyu/dnaStrand.js
@@ -0,0 +1,11 @@
+/*
+We are about to work of a Kata which is based on a simplified idea from biology.
+Here is the story
+DNA is made up of two strands that pair together, a bit like the two sides of a zipper.
+Each "letter" on one strand always matches a specific letter on the other strand:
+A-> T
+T->A
+C->G
+G->C
+
+*/
