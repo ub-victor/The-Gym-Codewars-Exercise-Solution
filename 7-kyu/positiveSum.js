43ed2a564cbdd514e92d161f766898a1936ee6b5
ub-victor <victoireushindi371@gmail.com>
2026-05-31 13:45:35 +0200

    Code war

--- 7-kyu/positiveSum.js
+++ 7-kyu/positiveSum.js
@@ -1,6 +1,6 @@
 const positiveSum = (arr) => {
     let result = 0;
-    for(let i=0; i< arr.length; arr ++){
+    for(let i=0; i< arr.length; i++){
         if(arr[i]>0){
             result += arr[i];
         }
