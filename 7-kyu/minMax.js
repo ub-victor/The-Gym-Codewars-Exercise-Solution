ea8c7cfca7bd8dd0bcb2759fd029e24c25582a3f
ub-victor <victoireushindi371@gmail.com>
2026-06-01 23:58:54 +0200

    Code war

--- 7-kyu/minMax.js
+++ 7-kyu/minMax.js
@@ -1,5 +1,6 @@
 const minMax = (arr) =>{
     // The spread operator ... "Unpacks" the array into individual elements.
     // Math.max(...arr) becomes Math.max(3,4,5,6,7,4,3,2,9,5)
-    
+    let result = [];
+    let left = Math.max(...arr);
 }
