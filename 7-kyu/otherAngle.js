9888668cafcf7ddb02678c42c34895260c88bf1d
ub-victor <victoireushindi371@gmail.com>
2026-06-04 15:34:24 +0200

    first non-constecutive

--- 7-kyu/otherAngle.js
+++ 7-kyu/otherAngle.js
@@ -1,3 +1,6 @@
 // A triangle always has 3 interior angles and their total sum is always: 180°
 // So if you are given 2 angles, the 3rd one is just what is missing to reach 180.
-// 
+// If:
+
+// angle1 = 60
+// angle2 = 50
