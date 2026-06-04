ccccfa5c6471fa2be4436abeceeb4fb33e852304
ub-victor <victoireushindi371@gmail.com>
2026-06-04 13:51:52 +0200

    first non-constecutive

--- 7-kyu/firstNonConsecutive.js
+++ 7-kyu/firstNonConsecutive.js
@@ -2,7 +2,7 @@
 
 const firstNonConsecutive = (arr)=>{
     for(let i= 1; i < arr.length; i++ ){
-        if(arr[i]!== arr[i -1] + 1 ){ // 3 !== 1 = 3 equal false as the previous index + 1 gives the next one  on the index 3it will be  
+        if(arr[i]!== arr[i -1] + 1 ){ // 3 !== 1 = 3 equal false as the previous index + 1 gives the next one  on the index 3 it will be 6 ==! arr[2] = 4 +1 = 5 which is true and it is the one pass the condition 
             return arr[i];
         }
     }
