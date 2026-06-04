2aea4e245b1919a4d9d064440f836e14d7837388
ub-victor <victoireushindi371@gmail.com>
2026-06-04 13:46:16 +0200

    first non-constecutive

--- 7-kyu/firstNonConsecutive.js
+++ 7-kyu/firstNonConsecutive.js
@@ -2,7 +2,7 @@
 
 const firstNonConsecutive = (arr)=>{
     for(let i= 1; i < arr.length; i++ ){
-        if(arr[i]!== arr[i -1] + 1 ){ // 3 !== [3-1] +1 = 3 equal false 
+        if(arr[i]!== arr[i -1] + 1 ){ // 3 !== 1 = 3 equal false 
             return arr[i];
         }
     }
