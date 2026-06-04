6415dcc524875cddf8c0c4514fd5ee0f3333ac7d
ub-victor <victoireushindi371@gmail.com>
2026-06-04 16:24:52 +0200

    between

--- 7-kyu/between.js
+++ 7-kyu/between.js
@@ -1,4 +1,10 @@
 const between = (a, b) => {
     let arr = [];
-    
+    if(a<b){
+        for(let i =a; a<=b; i++){
+            arr.push(i);
+        }
+    }else{
+        return "Check the value of a and b";
+    }
 }
