c8d31f34b2764fc51ef996fdae614f4b79eac120
ub-victor <victoireushindi371@gmail.com>
2026-06-05 16:52:34 +0200

    getGrade

--- 7-kyu/getGrade.js
+++ 7-kyu/getGrade.js
@@ -1,1 +1,12 @@
 // Grasshopper - Grade book
+
+function getGrade(s1, s2, s3) {
+  const average = (s1 + s2 + s3) / 3;
+
+  if (average >= 90) return 'A';
+  if (average >= 80) return 'B';
+  if (average >= 70) return 'C';
+  if (average >= 60) return 'D';
+
+  return 'F';
+}
