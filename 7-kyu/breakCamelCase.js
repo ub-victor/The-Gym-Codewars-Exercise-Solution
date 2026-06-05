1a364f6af521f7e62f2041cbdde7619777c1614b
ub-victor <victoireushindi371@gmail.com>
2026-06-05 03:46:12 +0200

    breakCamelCase

--- 7-kyu/breakCamelCase.js
+++ 7-kyu/breakCamelCase.js
@@ -1,6 +1,14 @@
 // Complete the solution so that the function will break up camel casing, using a space between words.
 
 const breakCamelCase = (str)=>{
+    /*
+    means:
+
+    [A-Z] → match any uppercase letter
+    () → capture the letter
+    g → find all matches
+    
+    */
     return str.replace(/([A-Z])/g , " $1");
 }
 
