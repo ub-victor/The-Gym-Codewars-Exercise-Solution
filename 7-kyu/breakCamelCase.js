508664907fff5df9d2ae7e9b6de74ad5ea3433ba
ub-victor <victoireushindi371@gmail.com>
2026-06-05 03:42:43 +0200

    breakCamelCase

--- 7-kyu/breakCamelCase.js
+++ 7-kyu/breakCamelCase.js
@@ -1,5 +1,7 @@
 // Complete the solution so that the function will break up camel casing, using a space between words.
 
 const breakCamelCase = (str)=>{
-    
+    return str.replace(/([A-Z])/g , " $1");
 }
+
+console.log(breakCamelCase("HelloWorldGys"))
