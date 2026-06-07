2df6b4690ece3b8eb756c86c02656dbfb4d17d26
ub-victor <victoireushindi371@gmail.com>
2026-06-07 16:11:46 +0200

    gettimestamp

--- 7-kyu/getTimestampsByDescription.js
+++ 7-kyu/getTimestampsByDescription.js
@@ -4,6 +4,12 @@ Imagine a security monitoring system.
 Whenever something happens, the system writes an event into an XML log file.
 
 eg: 
+        <event timestamp="1614285589">
+        <description>Intrusion detected</description>
+        </event>
 
+        <event timestamp="1614286432">
+            <description>Intrusion ended</description>
+        </event>
 
 */
