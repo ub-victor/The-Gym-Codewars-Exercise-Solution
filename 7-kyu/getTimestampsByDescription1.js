752b6f873f95ec32af629d2a43f352809ea3b4a2
ub-victor <victoireushindi371@gmail.com>
2026-06-09 13:39:14 +0200

    gettimestamp

--- 7-kyu/getTimestampsByDescription1.js
+++ 7-kyu/getTimestampsByDescription1.js
@@ -90,7 +90,7 @@ const getTimestampsByDescription = (xml, description)=>{
         const descriptionMatch =
             event.match(/<description>(.*?)<\/description/);
 
-        
+        const eventDescription = description[1];
     }
 
 }
