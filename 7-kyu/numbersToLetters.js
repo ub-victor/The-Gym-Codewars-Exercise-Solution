eb615238fb245a8bc779cf6d9c8081de76df1669
ub-victor <victoireushindi371@gmail.com>
2026-06-10 17:56:30 +0200

    Number to letters

--- 7-kyu/numbersToLetters.js
+++ 7-kyu/numbersToLetters.js
@@ -2,6 +2,8 @@
 Convert a string of numbers to a sentence. Each number represents a letter.
 Numbers in the string are separated by a space, and words in the sentence are separated by a plus character
 Conversion table : 1 = A, 2 = B
-eg: 
+eg: numbersToLetters('20 5 19 20+4 15 13 5') should return "TEST DOME"
+
+
 
 */
