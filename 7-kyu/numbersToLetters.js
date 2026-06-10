35de0391a8fcec803270a097c8d72437741e42e8
ub-victor <victoireushindi371@gmail.com>
2026-06-10 17:50:05 +0200

    Number to letters

--- 7-kyu/numbersToLetters.js
+++ 7-kyu/numbersToLetters.js
@@ -1,4 +1,4 @@
 /*
 Convert a string of numbers to a sentence. Each number represents a letter.
-Number
+Numbers in the string are separated by a space, and words in the sentence are separated by a plus character
 */
