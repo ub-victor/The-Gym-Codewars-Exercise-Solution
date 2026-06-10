a8d4ed14aef232a8b3fc0cb8e8d507fdc6ec4d33
ub-victor <victoireushindi371@gmail.com>
2026-06-10 17:57:47 +0200

    Number to letters

--- 7-kyu/numbersToLetters.js
+++ 7-kyu/numbersToLetters.js
@@ -4,6 +4,6 @@ Numbers in the string are separated by a space, and words in the sentence are se
 Conversion table : 1 = A, 2 = B
 eg: numbersToLetters('20 5 19 20+4 15 13 5') should return "TEST DOME"
 
-Here is how i thing to solve this
+Here is how I thing to solve this 
 
 */
