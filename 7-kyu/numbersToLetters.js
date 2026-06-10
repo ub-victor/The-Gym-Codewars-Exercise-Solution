b5b61d0270a20631c894955c36cf59cb04db3095
ub-victor <victoireushindi371@gmail.com>
2026-06-10 18:13:50 +0200

    Number to letters

--- 7-kyu/numbersToLetters.js
+++ 7-kyu/numbersToLetters.js
@@ -8,9 +8,9 @@ Here is how I thing to solve this
 Suppose somebody gives you:
 20 5 19 20+4 15 13 5
 
-step 1 : Separate the words where we found + we split with comma space
+step 1 : Separate the words where we found + we split with
 ["20 5 19 20", "4 15 13 5"] Now we know we have word 1 word 2
-
+split by spaces: ["20 5 19 20"]
 
 
 
