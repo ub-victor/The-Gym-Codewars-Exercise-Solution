a9a070ad6f539331c9ab6a65beacc09908e3cb18
ub-victor <victoireushindi371@gmail.com>
2026-06-13 18:57:46 +0200

    r beats s beats p beats r

--- 7-kyu/rsp.js
+++ 7-kyu/rsp.js
@@ -15,5 +15,15 @@ Examples:
 */
 
 const rsp = (p1, p2) => {
-    
+    if(p1 = p2) return "Draw!"
+
+    if(
+        (p1 === "scissors" && p2 === "paper") ||
+        (p1 === "rock" && p2 === "scissors") ||
+        (p1 === "paper" && p2 === "rock")
+    ){
+        return "Player 1 won!";
+    }else{
+        return "Player 2 won!"
+    }
 }
