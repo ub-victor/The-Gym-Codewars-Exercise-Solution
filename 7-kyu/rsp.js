4e88597257894835c781ac5538c3c436d26f3517
ub-victor <victoireushindi371@gmail.com>
2026-06-13 18:10:00 +0200

    r beats s beats p beats r

---
+++ 7-kyu/rsp.js
@@ -0,0 +1,16 @@
+/*
+Rules of the "Rock, Paper, Scissors" game are:
+
+Rock beats Scissors,
+Scissors beat Paper,
+Paper beats Rock,
+Two identical moves are a draw.
+Let's play! You will be given valid moves of two Rock, Paper, Scissors players, and have to return which player won: "Player 1 won!" for player 1, and "Player 2 won!" for player 2. In case of a draw return Draw!.
+
+Examples:
+"scissors",     "paper"     --> "Player 1 won!"
+"scissors",     "rock"      --> "Player 2 won!"
+"paper",        "paper"     --> "Draw!"
+
+
+*/
