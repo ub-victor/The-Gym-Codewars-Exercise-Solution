11e90d01deb0ce4d9eab195e8e6227bdc34ccbe7
ub-victor <victoireushindi371@gmail.com>
2026-06-15 20:09:50 +0200

    longestConsec

--- 7-kyu/longestConsec.js
+++ 7-kyu/longestConsec.js
@@ -7,6 +7,6 @@ k = 2
 The challenge asks:
     Take K consecutive strings from the array, join them together, and find the longest resulting string 
 Then what does Consecutive mean?
-    
+    Consecutive means next to each in the array
 
 */
