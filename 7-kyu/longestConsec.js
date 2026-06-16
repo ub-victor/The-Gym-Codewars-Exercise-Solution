aea6b6ca151a434dcdeadfea220a352ed80f05dc
ub-victor <victoireushindi371@gmail.com>
2026-06-17 00:02:14 +0200

    longestConsec

--- 7-kyu/longestConsec.js
+++ 7-kyu/longestConsec.js
@@ -14,6 +14,7 @@ but ["tree", "trashy"] is Not consecutive because "foling" is between them
 We will build alll possible groups 
     k = 2, we take 2 neighboring strings at a time
 Group 1: "tree" + "foling" = "treefoling" its length is 10
+Group 2: "foling" + "trashy" = "folingtrashy" it length is 12
 
 
 
