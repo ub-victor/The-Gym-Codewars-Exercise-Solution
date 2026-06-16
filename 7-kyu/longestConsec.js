a0403d2c5ed74a2b934b06b64228e327612a036c
ub-victor <victoireushindi371@gmail.com>
2026-06-17 00:03:09 +0200

    longestConsec

--- 7-kyu/longestConsec.js
+++ 7-kyu/longestConsec.js
@@ -15,7 +15,7 @@ We will build alll possible groups
     k = 2, we take 2 neighboring strings at a time
 Group 1: "tree" + "foling" = "treefoling" its length is 10
 Group 2: "foling" + "trashy" = "folingtrashy" it length is 12
-
+Group 3: ""trashy" + "blue" = "trashyblue" its lenghth is 10 
 
 
 
