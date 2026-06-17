26605b84cde5808a47ed75db09693e806b433583
ub-victor <victoireushindi371@gmail.com>
2026-06-17 15:16:05 +0200

    longestConsec

--- 7-kyu/slice.js
+++ 7-kyu/slice.js
@@ -8,4 +8,4 @@ const animals = ["ant", "bison", "camel", "duck", "elephant"];
 const slice0 = animals.slice(2) // from two to the  end
 console.log(slice0)
 
-const slice1 = animals.slice(2) 
+const slice1 = animals.slice(2,4) //4 not included
