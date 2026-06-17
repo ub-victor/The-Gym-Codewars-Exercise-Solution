03fedb8d2cbec47428c4e8fd4cec1d0b5e7e5adf
ub-victor <victoireushindi371@gmail.com>
2026-06-17 14:52:37 +0200

    longestConsec

--- 7-kyu/slice.js
+++ 7-kyu/slice.js
@@ -1,3 +1,3 @@
 /* 
-The slice() method of array instances return a shollow copy. of a portion of an array into a new array object selected from start to end
+The slice() method of array instances return a shollow copy. of a portion of an array into a new array object selected from start to end (end not included)
 */
