2ecb08b0a7fe7484d055a55eb081baac3c0f1619
ub-victor <victoireushindi371@gmail.com>
2026-06-17 15:13:00 +0200

    longestConsec

--- 7-kyu/slice.js
+++ 7-kyu/slice.js
@@ -1,3 +1,6 @@
 /* 
 The slice() method of array instances return a shollow copy. of a portion of an array into a new array object selected from start to end (end not included)
 */
+
+
+const animals = ["ant", "bison", "camel", "duck", "elephant"];
