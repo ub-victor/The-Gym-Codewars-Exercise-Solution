707f4363f9c0c7ac2be7f686d261077ce7035500
ub-victor <victoireushindi371@gmail.com>
2026-06-21 19:33:37 +0200

    past

--- 7-kyu/past.js
+++ 7-kyu/past.js
@@ -7,6 +7,6 @@ Your task is to return the time elapsed since midnight in milliseconds.
 1 h = 60mins 
     = 60mins = 3600 seconds
     = 3 600 seconds * 1000 = 3 600 000
-    = 3 600 000 + 3600 + 1000 =  3 600 4 600
+    = 3 600 000 + 3600 + 1000 =  3 600 4 600 
  
 */
