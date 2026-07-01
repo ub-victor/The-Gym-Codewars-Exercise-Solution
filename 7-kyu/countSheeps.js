b2eea4f7a523afbfe077aad93f9793d90e7d133d
ub-victor <victoireushindi371@gmail.com>
2026-07-01 16:33:21 +0200

    isValidWalk

--- 7-kyu/countSheeps.js
+++ 7-kyu/countSheeps.js
@@ -21,4 +21,4 @@ console.log(countSheeps([true,  true,  true,  false,
                         true,  false, true,  false,
                         true,  false, false, true ,
                         true,  true,  true,  true ,
-                          false, false, true,  true ]));
+                        false, false, true,  true ]));
